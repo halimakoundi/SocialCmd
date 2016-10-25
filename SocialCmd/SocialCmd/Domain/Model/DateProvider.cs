fac74444e22b29d4352168dedfacda836fcc2d68
Halima KOUNDI <hkoundi@gmail.com>
2016-10-25 13:45:01 +0100

    extract date provider

---
+++ SocialCmd/SocialCmd/Domain/Model/DateProvider.cs
@@ -0,0 +1,25 @@
+using System;
+
+static internal class DateProvider
+{
+    private static string TimelinePostDate(DateTime datePosted){
+			
+        var span = (DateTime.Now).Subtract ( datePosted );
+        var timeDifference = String.Empty;
+        if (span.Days > 0) {
+            timeDifference += $"{span.Days} day{(span.Days > 1 ? "s" : "")}";
+        }else if (span.Hours > 0) {
+            timeDifference += $"{span.Hours} hour{(span.Hours > 1 ? "s" : "")}";
+        }else if (span.Minutes > 0) {
+            timeDifference += $"{span.Minutes} minute{(span.Minutes > 1 ? "s" : "")}";
+        }else if (span.Seconds > 0) {
+            timeDifference += $"{span.Seconds} second{(span.Seconds > 1 ? "s" : "")}";;
+        }
+        if(!String.IsNullOrEmpty(timeDifference)){
+            timeDifference += " ago";
+        }else { 
+            timeDifference = "just now";
+        }
+        return timeDifference ;
+    }
+}
