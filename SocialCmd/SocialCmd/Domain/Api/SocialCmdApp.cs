dff918b373ecbdc5e5ac98e3a66329905264ff02
Halima KOUNDI <hkoundi@gmail.com>
2016-10-25 11:06:49 +0100

    introduce param and make method static

--- SocialCmd/SocialCmd/Domain/Api/SocialCmdApp.cs
+++ SocialCmd/SocialCmd/Domain/Api/SocialCmdApp.cs
@@ -36,7 +36,7 @@ namespace SocialCmd.Domain.Api
                     result = command.Execute();
                     break;
                 case CmdKey.Follow:
-                    result = UserFollowAnotherUser(commandDetails.UserName, commandDetails.UserNameToFollow);
+                    result = UserFollowAnotherUser(commandDetails.UserName, commandDetails.UserNameToFollow, _userRepository);
                     break;
                 case CmdKey.Read:
                     command = new ReadAllPostsCommand(commandDetails, _userRepository);
@@ -68,11 +68,11 @@ namespace SocialCmd.Domain.Api
                 throw new Exception("The command cannot be empty.");
         }
 
-        public CommandResponse UserFollowAnotherUser(string userName, string userNameToFollow)
+        public CommandResponse UserFollowAnotherUser(string userName, string userNameToFollow, UserRepository userRepository)
         {
             var result = new CommandResponse();
-            var user = _userRepository.FindUserBy(userName);
-            var usertoFollow = _userRepository.FindUserBy(userNameToFollow);
+            var user = userRepository.FindUserBy(userName);
+            var usertoFollow = userRepository.FindUserBy(userNameToFollow);
 
             if (user != null && usertoFollow != null)
             {
@@ -87,9 +87,11 @@ namespace SocialCmd.Domain.Api
 
         private static CommandResponse InvalidRequestFor(User usertoFollow)
         {
-            CommandResponse result = new CommandResponse();
-            result.Value = $"User{(usertoFollow != null ? " to follow" : "")} does not exist";
-            result.Success = false;
+            var result = new CommandResponse
+            {
+                Value = $"User{(usertoFollow != null ? " to follow" : "")} does not exist",
+                Success = false
+            };
             return result;
         }
     }
