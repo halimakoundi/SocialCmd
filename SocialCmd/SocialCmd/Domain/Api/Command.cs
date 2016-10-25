555a0e6de0059bf3e4cf9d7ac9e2554707484b86
Halima KOUNDI <hkoundi@gmail.com>
2016-10-25 12:11:14 +0100

    extract command factory

--- SocialCmd/SocialCmd/Domain/Api/Command.cs
+++ SocialCmd/SocialCmd/Domain/Api/Command.cs
@@ -1,29 +1,29 @@
-using SocialCmd;
-using SocialCmd.Domain.Api;
-
-static internal class Command
+namespace SocialCmd.Domain.Api
 {
-    public static ICommand CommandFrom(CommandDetails commandDetails, UserRepository userRepository)
+    internal static class Command
     {
-        ICommand command = null;
-        switch (commandDetails.CommandKey)
+        public static ICommand From(CommandDetails commandDetails, UserRepository userRepository)
         {
-            case CmdKey.Post:
-                command = new PostCommand(commandDetails, userRepository);
-                break;
-            case CmdKey.Follow:
-                command = new FollowUserCommand(commandDetails, userRepository);
-                break;
-            case CmdKey.Read:
-                command = new ReadAllPostsCommand(commandDetails, userRepository);
-                break;
-            case CmdKey.PrintWall:
-                command = new PrintWallCommand(commandDetails, userRepository);
-                break;
-            default:
-                command = new InvalidCommand();
-                break;
+            ICommand command = null;
+            switch (commandDetails.CommandKey)
+            {
+                case CmdKey.Post:
+                    command = new PostCommand(commandDetails, userRepository);
+                    break;
+                case CmdKey.Follow:
+                    command = new FollowUserCommand(commandDetails, userRepository);
+                    break;
+                case CmdKey.Read:
+                    command = new ReadAllPostsCommand(commandDetails, userRepository);
+                    break;
+                case CmdKey.PrintWall:
+                    command = new PrintWallCommand(commandDetails, userRepository);
+                    break;
+                default:
+                    command = new InvalidCommand();
+                    break;
+            }
+            return command;
         }
-        return command;
     }
 }
